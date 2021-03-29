dd825b7aba3a25497c8b508e049b66f748c26861
Charles Chen <charles@innotech.me>
2021-03-29 17:15:16 +0800

    feat 更新樣式

--- src/main/java/com/eachnow/linebot/domain/service/handler/command/impl/BookkeepingHandler.java
+++ src/main/java/com/eachnow/linebot/domain/service/handler/command/impl/BookkeepingHandler.java
@@ -8,16 +8,20 @@ import com.eachnow.linebot.common.po.CommandPO;
 import com.eachnow.linebot.common.util.DateUtils;
 import com.eachnow.linebot.common.util.ParamterUtils;
 import com.eachnow.linebot.domain.service.handler.command.CommandHandler;
-import com.linecorp.bot.model.action.Action;
-import com.linecorp.bot.model.action.MessageAction;
+import com.linecorp.bot.model.action.PostbackAction;
+import com.linecorp.bot.model.message.FlexMessage;
 import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TemplateMessage;
 import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.message.template.ConfirmTemplate;
+import com.linecorp.bot.model.message.flex.component.Box;
+import com.linecorp.bot.model.message.flex.component.Button;
+import com.linecorp.bot.model.message.flex.component.FlexComponent;
+import com.linecorp.bot.model.message.flex.component.Text;
+import com.linecorp.bot.model.message.flex.container.Bubble;
+import com.linecorp.bot.model.message.flex.container.FlexContainer;
+import com.linecorp.bot.model.message.flex.unit.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.Arrays;
@@ -58,24 +62,21 @@ public class BookkeepingHandler implements CommandHandler {
         }
         String data = commandPO.getCommand() + ParamterUtils.CONTACT + typeName + ParamterUtils.CONTACT +
                 amount + ParamterUtils.CONTACT + currencyEnum.getName() + ParamterUtils.CONTACT;
-//        List<FlexComponent> bodyContents = Arrays.asList(
-//                Text.builder().text("金額: " + amount).size(FlexFontSize.LG).build(),
-//                Text.builder().text("幣值: " + currencyEnum.getName()).size(FlexFontSize.LG).build(),
-//                Text.builder().text("類型: " + typeName).size(FlexFontSize.LG).build()
-//        );
-//        Box body = Box.builder().layout(FlexLayout.VERTICAL).contents(bodyContents).margin(FlexMarginSize.SM).build();
-//        List<FlexComponent> footerContents = Arrays.asList(
-//                Button.builder().style(Button.ButtonStyle.PRIMARY).height(Button.ButtonHeight.SMALL).action(PostbackAction.builder().label("確定").data(data + CONFIRM).build()).build(),
-//                Button.builder().style(Button.ButtonStyle.SECONDARY).height(Button.ButtonHeight.SMALL).action(PostbackAction.builder().label("取消").data(data + CANCEL).build()).build()
-//        );
-//        List<FlexComponent> headerContents = Arrays.asList(Text.builder().text("請問輸入正確嗎?").size(FlexFontSize.LG).weight(Text.TextWeight.BOLD).align(FlexAlign.CENTER).color("#ffffff").build());
-//        Box header = Box.builder().layout(FlexLayout.VERTICAL).contents(headerContents).paddingAll(FlexPaddingSize.MD).backgroundColor("#29bae6").build();
-//        Box footer = Box.builder().layout(FlexLayout.HORIZONTAL).contents(footerContents).spacing(FlexMarginSize.MD).build();
-//        FlexContainer contents = Bubble.builder().header(header).hero(null).body(body).footer(footer).build();
-//        return new FlexMessage("記帳確認", contents);
-        List<Action> actions = Arrays.asList(new MessageAction("確定", data + CONFIRM), new MessageAction("取消", data + CANCEL));
-        ConfirmTemplate confirmTemplate = new ConfirmTemplate("請問輸入正確嗎?\n金額: " + amount + "\n幣值: " + currencyEnum.getName() + "\n類型: " + typeName, actions);
-        return new TemplateMessage("記帳確認", confirmTemplate);
+        List<FlexComponent> bodyContents = Arrays.asList(
+                Text.builder().text("金額: " + amount).size(FlexFontSize.LG).build(),
+                Text.builder().text("幣值: " + currencyEnum.getName()).size(FlexFontSize.LG).build(),
+                Text.builder().text("類型: " + typeName).size(FlexFontSize.LG).build()
+        );
+        Box body = Box.builder().layout(FlexLayout.VERTICAL).contents(bodyContents).margin(FlexMarginSize.SM).build();
+        List<FlexComponent> footerContents = Arrays.asList(
+                Button.builder().style(Button.ButtonStyle.PRIMARY).height(Button.ButtonHeight.SMALL).action(PostbackAction.builder().label("確定").data(data + CONFIRM).build()).build(),
+                Button.builder().style(Button.ButtonStyle.SECONDARY).height(Button.ButtonHeight.SMALL).action(PostbackAction.builder().label("取消").data(data + CANCEL).build()).build()
+        );
+        List<FlexComponent> headerContents = Arrays.asList(Text.builder().text("請問輸入正確嗎?").size(FlexFontSize.LG).weight(Text.TextWeight.BOLD).align(FlexAlign.CENTER).color("#ffffff").build());
+        Box header = Box.builder().layout(FlexLayout.VERTICAL).contents(headerContents).paddingAll(FlexPaddingSize.MD).backgroundColor("#29bae6").build();
+        Box footer = Box.builder().layout(FlexLayout.HORIZONTAL).contents(footerContents).spacing(FlexMarginSize.MD).build();
+        FlexContainer contents = Bubble.builder().header(header).hero(null).body(body).footer(footer).build();
+        return new FlexMessage("記帳確認", contents);
     }
 
     public String getFormat() {
@@ -95,11 +96,4 @@ public class BookkeepingHandler implements CommandHandler {
         return Pattern.matches("[0-9]*(\\.?)[0-9]*", input);
     }
 
-//    @PostConstruct
-//    private void test() {
-//        String text = "記 晚餐 180";
-//        CommandPO commandPO = CommandPO.builder().userId("userId").text(text)
-//                .command(ParamterUtils.parseCommand(text)).params(ParamterUtils.listParameter(text)).build();
-//        this.execute(commandPO);
-//    }
 }
