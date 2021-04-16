e9590a7b2e2b072c1af26f30d4394a68b16216ea
Charles Chen <charles@innotech.me>
2021-04-16 14:41:12 +0800

    feat 更新annotation

--- src/main/java/com/eachnow/linebot/common/po/DatetimepickerPO.java
+++ src/main/java/com/eachnow/linebot/common/po/DatetimepickerPO.java
@@ -1,11 +1,9 @@
 package com.eachnow.linebot.common.po;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 public class DatetimepickerPO {
     private String date;
