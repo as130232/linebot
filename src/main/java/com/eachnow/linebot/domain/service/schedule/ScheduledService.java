f06ee9a936a616d40469da9682223eb0eda0fadc
Charles Chen <charles@innotech.me>
2021-04-27 15:37:34 +0800

    feat 更新降雨機率

--- src/main/java/com/eachnow/linebot/domain/service/schedule/ScheduledService.java
+++ src/main/java/com/eachnow/linebot/domain/service/schedule/ScheduledService.java
@@ -54,19 +54,19 @@ public class ScheduledService {
         return CRON_EXECUTE;
     }
 
-//    @Scheduled(cron = "${schedule.beauty.cron}")
+    @Scheduled(cron = "${schedule.beauty.cron}")
     public void beautyCrawler() {
         if (!CRON_EXECUTE)
             return;
         log.info("[schedule]準備爬取表特版。time:{}", new Date());
-        beautyCrawlerService.crawler(3);
+        beautyCrawlerService.crawler(2);
         log.info("[schedule]爬取表特版，完成。time:{}", new Date());
     }
 
     /**
      * 下雨警報
      */
-    @Scheduled(cron = "0 0 8,23 * * ?")
+    @Scheduled(cron = "0 0 7,23 * * ?")
     public void rainAlarm() {
         String loactionName = "臺北市";
         WeatherResultPO po = openWeatherService.getWeatherInfo(loactionName, WeatherElementEnum.POP.getElement());
@@ -75,18 +75,14 @@ public class ScheduledService {
         Integer currentHour = zonedDateTime.getHour();
         for (WeatherElementPO weatherElementPO : po.getRecords().getLocation().get(0).getWeatherElement()) {
             for (TimePO timePO : weatherElementPO.getTime()) {   //取得隔天早上06:00 ~ 18:00 的機率
-                ZonedDateTime startTime = DateUtils.parseDate(timePO.getStartTime(), DateUtils.yyyyMMddHHmmssDash);
-                if (currentHour == 23 && (currentDay + 1) == startTime.getDayOfMonth()
-                        || currentHour == 8 && currentDay == startTime.getDayOfMonth()) {
-                    if (timePO.getStartTime().contains("06:00:00") && timePO.getEndTime().contains("18:00:00")) {
-                        Integer unit = Integer.valueOf(timePO.getParameter().getParameterUnit());
-                        //降雨機率大於60% 則通知
-                        if (unit >= 60) {
-                            lineNotifySender.send(lineConfig.getLineNotifyKeyOwn(), DateUtils.parse(timePO.getStartTime(), DateUtils.yyyyMMddHHmmssDash, DateUtils.yyyyMMddHHmmDash) + " - 18:00，降雨機率為: {unit}%，請記得帶傘。".replace("{unit}", unit.toString()));
-                            break;
-                        }
-                    }
+//                if (timePO.getStartTime().contains("06:00:00") && timePO.getEndTime().contains("18:00:00")) {
+                Integer unit = Integer.valueOf(timePO.getParameter().getParameterUnit());
+                //降雨機率大於70% 則通知
+                if (unit >= 70) {
+                    lineNotifySender.send(lineConfig.getLineNotifyKeyOwn(), DateUtils.parse(timePO.getStartTime(), DateUtils.yyyyMMddHHmmssDash, DateUtils.yyyyMMddHHmmDash) + " - 18:00，降雨機率為: {unit}%，請記得帶傘。".replace("{unit}", unit.toString()));
+                    break;
                 }
+//                }
             }
         }
     }
