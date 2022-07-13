c907e985113d2a646f858575944740c98e0fc09c
Charles Chen <charles@innotech.me>
2022-07-13 15:57:15 +0800

    test2 by heroku cli

--- src/main/java/com/eachnow/linebot/domain/service/handler/command/impl/StockHandler.java
+++ src/main/java/com/eachnow/linebot/domain/service/handler/command/impl/StockHandler.java
@@ -400,7 +400,6 @@ public class StockHandler implements CommandHandler {
         }).collect(Collectors.toList());
         bodyComponent.addAll(listComponent);
         Box body = Box.builder().layout(FlexLayout.VERTICAL).contents(bodyComponent).paddingAll(FlexPaddingSize.MD).paddingTop(FlexPaddingSize.NONE).build();
-
         String label = DateUtils.parseDate(date, DateUtils.yyyyMMdd, DateUtils.yyyyMMddSlash);
         String datetimepickerData = commandPO.getCommand() + ParamterUtils.CONTACT + date + ParamterUtils.CONTACT;
         Box dateButton = Box.builder().layout(FlexLayout.HORIZONTAL).contents(Button.builder().flex(2).height(Button.ButtonHeight.SMALL)
