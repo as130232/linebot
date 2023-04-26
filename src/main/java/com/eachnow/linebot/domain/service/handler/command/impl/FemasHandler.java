b9ad6c93ee81313d9f76c79841da7efd47687461
Charles <charles.chen0123@gmail.com>
2023-04-26 14:35:07 +0800

    update punch handler

--- src/main/java/com/eachnow/linebot/domain/service/handler/command/impl/FemasHandler.java
+++ src/main/java/com/eachnow/linebot/domain/service/handler/command/impl/FemasHandler.java
@@ -4,6 +4,7 @@ import com.eachnow.linebot.common.annotation.Command;
 import com.eachnow.linebot.common.po.CommandPO;
 import com.eachnow.linebot.common.po.femas.FemasPunchRecordPO;
 import com.eachnow.linebot.common.util.DateUtils;
+import com.eachnow.linebot.domain.service.FemasService;
 import com.eachnow.linebot.domain.service.LocalCacheService;
 import com.eachnow.linebot.domain.service.handler.command.CommandHandler;
 import com.linecorp.bot.model.message.Message;
@@ -15,16 +16,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Command({"打卡"})
 public class FemasHandler implements CommandHandler {
     private final LocalCacheService localCacheService;
-
+    private final FemasService femasService;
     @Autowired
-    public FemasHandler(LocalCacheService localCacheService) {
+    public FemasHandler(LocalCacheService localCacheService, FemasService femasService) {
         this.localCacheService = localCacheService;
+        this.femasService = femasService;
     }
 
     @Override
     public Message execute(CommandPO commandPO) {
         switch (commandPO.getCommand()) {
             case "打卡":
+                femasService.remindPunchOut();
                 FemasPunchRecordPO po = localCacheService.getPunchRecord(DateUtils.getCurrentDate());
                 String sb = "上班時間：" + po.getStartTime() + "\n" +
                         "下班時間：" + po.getEndTime();
