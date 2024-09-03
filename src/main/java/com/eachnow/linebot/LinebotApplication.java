4f28e101cee35fc6af5c4ea6156e3a9c0de7944d
Charles <btp_charles@photons.com.tw>
2024-09-03 17:14:18 +0800

    chore

--- src/main/java/com/eachnow/linebot/LinebotApplication.java
+++ src/main/java/com/eachnow/linebot/LinebotApplication.java
@@ -3,25 +3,13 @@ package com.eachnow.linebot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 @EnableScheduling
-public class LinebotApplication implements CommandLineRunner{
+public class LinebotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LinebotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LinebotApplication.class, args);
+    }
 
-	@Autowired
-	private RequestMappingHandlerMapping handlerMapping;
-	@Override
-	public void run(String... args) throws Exception {
-		handlerMapping.getHandlerMethods().forEach((key, value) -> {
-			System.out.println(key + " -> " + value);
-		});
-	}
 }
